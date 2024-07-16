b13355edd745c58f5ad92067fe8d3683a0078fa0
Ansh98755 <ayushmehendiratta3390@gmail.com>
2024-07-16 17:56:57 +0530

    completed the login activity

--- app/src/main/java/com/example/foodiecafe/MainActivity.java
+++ app/src/main/java/com/example/foodiecafe/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.foodiecafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Please login", Toast.LENGTH_SHORT).show();
+                Intent it =new Intent(MainActivity.this , SigninActivity.class);
+                startActivity(it);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Register your account", Toast.LENGTH_LONG).show();
+                Intent it = new Intent(MainActivity.this , SignupActivity.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
